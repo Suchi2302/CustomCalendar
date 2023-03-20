fb1bb2b4f2ab8692c5f60950c11961e121c2c922
Suchi465 <107851318+Suchi465@users.noreply.github.com>
2023-03-20 16:09:10 +0530

    usage of only calendar component

---
+++ customcalendar/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CalendarDashboard from "./CalendarDashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <CalendarDashboard />
+    </div>
+  );
+}
+
+export default App;
